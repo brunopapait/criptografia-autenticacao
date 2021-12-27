9bcd08e8e29b854c1f1f81daedaedda39f0ee995
brunopapait <brunohpapait@gmail.com>
2021-12-27 00:37:10 -0300

    feat: add dependencie spring-boot-starter-security and exclude SecurityAutoConfiguration.class of method main

--- src/main/java/br/papait/bruno/autenticacaojwt/AutenticacaoJwtApplication.java
+++ src/main/java/br/papait/bruno/autenticacaojwt/AutenticacaoJwtApplication.java
@@ -2,8 +2,9 @@ package br.papait.bruno.autenticacaojwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class AutenticacaoJwtApplication {
 
   public static void main(String[] args) {
