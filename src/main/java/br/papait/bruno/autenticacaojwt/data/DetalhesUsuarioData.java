03da88960867f4a1194b49c8cab426d71a018b07
brunopapait <brunohpapait@gmail.com>
2021-12-27 15:45:55 -0300

    feat: add methods to get user details of UserDetailsService and UserDetails of spring security

--- src/main/java/br/papait/bruno/autenticacaojwt/data/DetalhesUsuarioData.java
+++ src/main/java/br/papait/bruno/autenticacaojwt/data/DetalhesUsuarioData.java
@@ -1,4 +1,54 @@
 package br.papait.bruno.autenticacaojwt.data;
 
-public class DetalhesUsuarioData {
+import br.papait.bruno.autenticacaojwt.entity.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Optional;
+
+public class DetalhesUsuarioData implements UserDetails {
+
+  private Optional<Usuario> usuario;
+
+  public DetalhesUsuarioData(Optional<Usuario> usuario) {
+    this.usuario = usuario;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return new ArrayList<>();
+  }
+
+  @Override
+  public String getPassword() {
+    return usuario.orElse(new Usuario()).getPassword();
+  }
+
+  @Override
+  public String getUsername() {
+    return usuario.orElse(new Usuario()).getLogin();
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
---
+++ src/main/java/br/papait/bruno/autenticacaojwt/service/DetalheUsuarioService.java
@@ -0,0 +1,29 @@
+package br.papait.bruno.autenticacaojwt.service;
+
+import br.papait.bruno.autenticacaojwt.data.DetalhesUsuarioData;
+import br.papait.bruno.autenticacaojwt.entity.Usuario;
+import br.papait.bruno.autenticacaojwt.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class DetalheUsuarioService implements UserDetailsService {
+
+  private UsuarioRepository usuarioRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+    Optional<Usuario> usuario = usuarioRepository.findByLogin(username);
+
+    if (usuario.isEmpty()) {
+      throw new UsernameNotFoundException("Usuário " + username + " não encontrado");
+    }
+
+    return new DetalhesUsuarioData(usuario);
+  }
+}
